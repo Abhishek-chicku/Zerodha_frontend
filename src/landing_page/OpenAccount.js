bbb8aae4e925f75b20c49f626b7959f9f21db6d9
Abhishek Kumar <as340490@gmail.com>
2025-07-18 01:26:21 +0530

    Your changes for home component

---
+++ src/landing_page/OpenAccount.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function OpenAccount() {
+  return (
+    <div class="container p-5 mb-5">
+      <div class="row text-center">
+        <h1 class="mt-5">Open a Zerodha account</h1>
+        <p className="mt-2">
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+          F&O trades.
+        </p>
+        <button
+          class="p-2 btn btn-primary fs-5 mt-4"
+          style={{ width: "20%", margin: "0 auto" }}
+        >
+          Sign up Now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default OpenAccount;
---
+++ src/landing_page/about/Team.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Team() {
+  return (
+    <div className="container">
+      <div className="row p-3">
+        <h1 className="text-center">People</h1>
+      </div>
+
+      <div
+        className="row p-3 text-center mb-5 "
+        style={{
+          lineHeight: "1.8",
+          fontSize: "1.1em",
+          marginLeft: "3em",
+        }}
+      >
+        <div className="col-6 p-3 mt-4">
+          <img
+            src="./media/images/nithinkamath.jpg"
+            style={{
+              borderRadius: "100%",
+              width: "50%",
+            }}
+          ></img>
+          <h4 className="mt-3">Nithin Kamath</h4>
+          <h6 className="mt-3">Founder, CEO</h6>
+        </div>
+        <div className="col-6 p-3 mt-5">
+          <p>
+            Nithin bootstrapped and founded Zerodha in 2010 to overcome the
+            hurdles he faced during his decade long stint as a trader. Today,
+            Zerodha has changed the landscape of the Indian broking industry.
+          </p>
+          <p>
+            He is a member of the SEBI Secondary Market Advisory Committee
+            (SMAC) and the Market Data Advisory Committee (MDAC).
+          </p>
+          <p>Playing basketball is his zen.</p>
+          <p>
+            Connect on <a href="">Homepage / TradingQnA / Twitter</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Team;
