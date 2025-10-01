4e848ec83e53ec8c0c8949d45b81576fa41ca13a
Abhishek Kumar <159581579+Abhishek-chicku@users.noreply.github.com>
2025-10-01 06:31:32 +0530

    Update Envoirment.js

--- src/landing_page/Envoirment.js
+++ src/landing_page/Envoirment.js
@@ -1,11 +1,4 @@
-
-
-const IS_PROD = process.env.NODE_ENV === "production"; 
-
 const Server = IS_PROD
   ? "https://zerodha-backend-7.onrender.com/api/auth"
-  : "http://localhost:4000/api/auth";                
-
+  : "http://localhost:4000/api/auth";
 export default Server;
-
-
